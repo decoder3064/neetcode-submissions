819b200c7d93a66eb68e6ecf2e041fc57f56cbd9
David Reyes <139591913+decoder3064@users.noreply.github.com>
2026-09-23 23:02:23 -0700

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.py
@@ -0,0 +1,44 @@
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        hmap = {}
+        counts = [[]] 
+
+        
+        for i in range(len(nums)): 
+            counts.append([])
+            if nums[i] in hmap: 
+                hmap[nums[i]] +=1
+            else:
+                hmap[nums[i]] = 1
+
+        for key,val in hmap.items():
+            counts[val].append(key)
+
+
+        
+        toReturn = []
+
+        for i in range(len(counts), 0, -1): 
+            for j in range(len(counts[i-1])):
+                print(counts[i-1])
+                toReturn.append(counts[i-1][j])
+                if len(toReturn) == k:
+                    return toReturn
+                
+
+        return []
+                
+
+                
+
+
+
+            
+
+        
+
+
+        
